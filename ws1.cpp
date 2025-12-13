654e1a66680419edc3fe737b29afa4a9fb452fc8
6804062611149 <s6804062611149@email.kmutnb.ac.th>
2025-12-13 15:17:28 +0700

    removed variable(max).cpp
    the variable is no longer of use in the code!

--- ws1.cpp
+++ ws1.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main () {
-	int n1, n2, n3, n4, max;
+	int n1, n2, n3, n4;
 	scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
 	((((n1 < n2) && (n1 > n3) && (n1 > n4)) || ((n1 < n3) && (n1 > n2) && (n1 > n4)) || ((n1 < n4) && (n1 > n2) && (n1 > n3)))? printf("%d",n1):
 	(((n2 < n1) && (n2 > n3) && (n2 > n4)) || ((n2 < n3) && (n2 > n1) && (n2 > n4)) || ((n2 < n4) && (n2 > n1) && (n2 > n3)))? printf("%d",n2):
 	(((n3 < n1) && (n3 > n2) && (n3 > n4)) || ((n3 < n2) && (n3 > n1) && (n3 > n4)) || ((n3 < n4) && (n3 > n1) && (n3 > n2)))? printf("%d",n3):
 	printf("%d",n4));
 }
+
